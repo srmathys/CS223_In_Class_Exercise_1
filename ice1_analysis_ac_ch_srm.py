fe971b41737d90ae7b7a2d3718ff1b08096efd05
Sharon Mathys <57904784+srmathys@users.noreply.github.com>
2026-09-10 08:44:41 -0700

    Update ice1_analysis_ac_ch_srm.py

--- ice1_analysis_ac_ch_srm.py
+++ ice1_analysis_ac_ch_srm.py
@@ -8,4 +8,14 @@ import anndata as ad
 #Loads the .h5ad file into an AnnData object
 adata =  ad.read_h5ad("./data/pbmc_sample.h5ad")
 
-def filter_mt_
+def filter_mt_cells(anndata_obj, mt_exp_lvl_theshold, gene_exp_threshold):
+
+
+
+
+
+
+if __name__ == "__main__": 
+  main()
+else:
+  print("<module name> : Is intended to be executed and not imported.")
