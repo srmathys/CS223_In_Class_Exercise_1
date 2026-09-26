9b5b544f5951cbb5a3975642888d0bfbfc6d45d7
Sharon Mathys <srmathys@gmail.com>
2026-09-25 23:34:07 -0700

    Added selection sort and insert sorts which work with columns, ascending and descending

--- ice1_analysis_ac_ch_srm.py
+++ ice1_analysis_ac_ch_srm.py
@@ -2,10 +2,23 @@
 # ice1_analysis
 
 from sort_and_search_funs import *
-from util_funs import *
-
+#from util_funs import
+import pandas as pd
 import anndata as ad
 #Loads the .h5ad file into an AnnData object
-adata =  ad.read_h5ad("./data/pbmc_sample.h5ad")
+#adata =  ad.read_h5ad("./data/pbmc_sample.h5ad")
 
-def filter_mt_
+#def filter_mt_
+
+# testing the sort
+
+
+data = {
+    'Gene' : ['CTCF','HTT','DND','KCQN1'],
+    'Section': [19,12,8,11],
+    'Average':[8.7,8,7.5,9]
+}
+df = pd.DataFrame(data)
+
+insert_sort_iter(df,2,asc = False)
+print(df)
--- sort_and_search_funs.py
+++ sort_and_search_funs.py
@@ -1,23 +1,74 @@
 #  coding: utf-8
 # Sort and search functions
 import pandas as pd
+# note: A.iat gets a single value using integer positioning in pandas
 
-def insert_sort_iter(A: pd.DataFrame,col: int,asc = True):
+def insert_sort_iter(A,col = 0,asc = True): #A is a dataframe, col is number
+    for i in range(1,A.iloc[:,col].size):
+        key = A.iat[i,col]
+        j = i - 1
+    if asc == True:
+        while j >= 0 and key < A.iat[j,col]:
+            A.iloc[[j+1,j]] = A.loc[[j,j+1]].values #if key is less than swap
+            j -= 1
+    else:
+        while j >= 0 and key > A.iat[j,col]:
+            A.iloc[[j+1,j]] = A.loc[[j,j+1]].values
+            j -= 1
+# limitations: I don't know how to shift in pandas the way it would be done in a list
 
-def insert_sort_rec(A: pd.DataFrame,col: int,asc = True):
+#def insert_sort_rec(A: pd.DataFrame,col: int,asc = True):
 
-def select_sort_iter(A: pd.DataFrame,col: int,asc = True):
+def selection_sort_iter(A,col = 0,asc = True):
+    length = A.iloc[:,col].size
+    if asc == True:
+        for i in range(length -1):
+            # finding the minimum element in the unsorted sublist from a dataframe column
+            # once found, it can be swapped with the index of i
+            min = i
+            for j in range(i+1,length):
+                if A.iat[j,col] < A.iat[min,col]:
+                    min = j
+            A.iloc[[i,min]] = A.iloc[[min,i]].values
+    else:
+        for i in range(length - 1): #same thing, but with max
+            max = i
+            for j in range(i+1,length):
+                if A.iat[j,col] > A.iat[max,col]:
+                    max = j
+            A.iloc[[i,max]] = A.iloc[[max,i]].values
 
-def select_sort_rec(A: pd.DataFrame,col: int,asc = True):
+def selection_sort_rec(A,col = 0,asc = True, i = 0):
+    length = A.iloc[:,col].size
+    if asc == True:
+        min = i
+        for j in range(i + 1, length):
+            if A.iat[j, col] < A.iat[min, col]:
+                min = j
+        A.iloc[[i, min]] = A.iloc[[min, i]].values
 
-def merge_sort_iter(A: pd.DataFrame,col: int,asc = True):
+        if i+1 < length:
+            selection_sort_rec(A,col, asc = True, i = i+1)
+    else:
+        max = i
+        for j in range(i + 1, length):
+            if A.iat[j, col] > A.iat[max, col]:
+                max = j
+        A.iloc[[i, max]] = A.iloc[[max, i]].values
+        if i+1 < length:
+            selection_sort_rec(A,col, asc = False, i = i+1)
 
-def merge_sort_rec(A: pd.DataFrame,col: int,asc = True):
+#def merge_sort_iter(A: pd.DataFrame,col: int,asc = True):
 
-def quick_sort_iter(A: pd.DataFrame,col: int,asc = True):
+#def merge_sort_rec(A: pd.DataFrame,col: int,asc = True):
 
-def quick_sort_rec(A: pd.DataFrame,col: int,asc = True):
+#def quick_sort_iter(A: pd.DataFrame,col: int,asc = True):
 
-def bin_search_iter(A: pd.DataFrame,col: int,asc = True):
+#def quick_sort_rec(A: pd.DataFrame,col: int,asc = True):
 
-def bin_search_rec(A: pd.DataFrame,col: int,asc = True):
+#def bin_search_iter(A: pd.DataFrame,col: int,asc = True):
+
+#def bin_search_rec(A: pd.DataFrame,col: int,asc = True):
+
+if __name__ == "__main__":
+    print("<module name> : Is intended to be imported and not executed.")
